3539f9525527527c3a1702396597a7357528615d
Muroslav <muroslavgoyk12@gmail.com>
2025-09-08 14:39:34 +0300

    1

--- task-3.js
+++ task-3.js
@@ -5,6 +5,6 @@ function getElementWidth (content, padding, border) {
     let total = contentNum + 2 * paddingNum + 2 * borderNum
     console.log(total)
 }
-console.log(getElementWidth("50px", "8px", "4px"))
+console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
