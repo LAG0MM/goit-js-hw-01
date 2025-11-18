7c985d08334b6a06d59e01fdcb7ffac055e7a3a5
LAG0MM <166709774+LAG0MM@users.noreply.github.com>
2025-11-18 17:28:09 +0100

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
-function getElementWidth (content, padding, border) {
-    let paddingNum = parseFloat(padding)
-    let borderNum = parseFloat(border)
-    let contentNum = parseFloat(content)
-    let total = contentNum + 2 * paddingNum + 2 * borderNum
-    console.log(total)
+function getElementWidth(content, padding, border) {
+  const paddingNum = parseFloat(padding);
+  const borderNum = parseFloat(border);
+  const contentNum = parseFloat(content);
+
+  const total = contentNum + 2 * paddingNum + 2 * borderNum;
+  return total;
 }
+
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
